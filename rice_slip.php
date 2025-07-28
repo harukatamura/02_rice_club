f52f5d6294f9f79f0cf300cb704a79ac84d3259e
harukatamura <j-system@jemtc.jp>
2025-07-28 12:23:43 +0900

    伝票発行画面作成

--- rice_slip.php
+++ rice_slip.php
@@ -64,23 +64,17 @@
 	else {
 		$p_display = 1;
 	}
-	$p_way["元払"] = 0;
-	$p_way["代引"] = 2;
-	$seikyu_code["本部"] = "0529368887";
-	$seikyu_code["補修センター"] = "052325741205";
 
 	$g_factory = "本部";
 	//エクセル出力処理
 	$outputno = $_POST['outputno'];
-	$outputno2 = $_POST['outputno'];
 	// 佐川伝票出力
 	if(isset($_POST['output_btn']) && $outputno != "") {
 		$comm->ouputlog("Excel出力 実行", $prgid, SYS_LOG_TYPE_INFO);
 		$reader = PHPExcel_IOFactory::createReader("Excel2007");
-		$book = $reader->load("./okurijo_torikomi_format.xlsx");
+		$book = $reader->load("./rice_sagawa_template.xlsx");
 		$sheet = $book->getSheetByName("佐川");
 		$chksheet = $book->getSheetByName("リスト");
-		$date_chksheet = $book->getSheetByName("リスト_日付指定");
 		//フラグを立てて出力するデータを取得
 		foreach($outputno as $value){
 			$query = "UPDATE rice_shipment ";
@@ -97,11 +91,9 @@
 		$i = 1;
 		$j = 3; // 開始行
 		$t = 0;
-		$g_modelnum = "";
-		$g_cash = "";
-		$g_g_staff = "";
-		$g_buynum = "";
-		$p_chksheet = $chksheet;
+		$g_category = "";
+		$g_cash = 0;
+		$g_buynum = 0;
 		//出力対象のデータを取得
 		$query = "SELECT A.ship_idxnum, A.tanka, A.category, A.weight, A.delivery_date, A.specified_times";
 		$query .= " ,C.name, C.company, C.phonenum1, C.postcd1, C.postcd2, C.address1, C.address2, C.address3, C.p_way ";
@@ -109,23 +101,17 @@
 		$query .= " LEFT OUTER JOIN php_rice_subscription B ON A.subsc_idxnum=B.subsc_idxnum ";
 		$query .= " LEFT OUTER JOIN php_rice_personal_info C ON B.personal_idxnum=C.idxnum ";
 		$query .= " WHERE A.output_flg = 3 AND A.stopflg = 0";
-		$query .= " ORDER BY C.postcd1, C.postcd2";
+		$query .= " ORDER BY A.category, A.weight, C.postcd1, C.postcd2";
 		$comm->ouputlog("データ抽出 実行", $prgid, SYS_LOG_TYPE_INFO);
 		$comm->ouputlog($query, $prgid, SYS_LOG_TYPE_DBUG);
 		if (!($rs = $db->query($query))) {
 			$comm->ouputlog("☆★☆データ追加エラー☆★☆ " . $db->errno . ": " . $db->error, $prgid, SYS_LOG_TYPE_ERR);
 		}
 		while($row = $rs->fetch_array()){
+			++$i;
+			++$j;
+			$j2 = $j - 1;
 			//佐川シートのセルに値をセット
-			if($row['deliveryday'] <> "0000-00-00"){
-				$sheet->setCellValueByColumnAndRow(44, $i, date('Y/n/j', strtotime($row['deliveryday'])));
-				if(strtotime($row['deliveryday'].'- 7 day') > strtotime(date('Ymd'))) {
-					$sheet->setCellValueByColumnAndRow(44, $i, date('Y/n/j',strtotime($row['deliveryday'].'-7day')));
-				}
-			}
-			if($row['deliverytime'] <> "指定なし"){
-				$sheet->setCellValueExplicitByColumnAndRow(45, $i, $row['deliverytime'], PHPExcel_Cell_DataType::TYPE_STRING);
-			}
 			$sheet->setCellValueExplicitByColumnAndRow(2, $i, $row['phonenum1'], PHPExcel_Cell_DataType::TYPE_STRING);
 			$sheet->setCellValueByColumnAndRow(3, $i, $row['postcd1']."-".$row['postcd2']);
 			$sheet->setCellValueByColumnAndRow(4, $i, $row['address1']);
@@ -133,70 +119,66 @@
 			$sheet->setCellValueByColumnAndRow(6, $i, $row['address3']);
 			$sheet->setCellValueByColumnAndRow(8, $i, $row['company']);
 			$sheet->setCellValueByColumnAndRow(7, $i, $row['name']);
-			$sheet->setCellValueExplicitByColumnAndRow(14, $i, "052-936-8887", PHPExcel_Cell_DataType::TYPE_STRING);
+			$sheet->setCellValueExplicitByColumnAndRow(14, $i, "050-5272-9665", PHPExcel_Cell_DataType::TYPE_STRING);
 			$sheet->setCellValueExplicitByColumnAndRow(16, $i, "293688870000", PHPExcel_Cell_DataType::TYPE_STRING);
+			$sheet->setCellValueExplicitByColumnAndRow(17, $i, "050-5272-9665", PHPExcel_Cell_DataType::TYPE_STRING);
 			$sheet->setCellValueExplicitByColumnAndRow(18, $i, "461-0011", PHPExcel_Cell_DataType::TYPE_STRING);
 			$sheet->setCellValueByColumnAndRow(19, $i, "愛知県名古屋市東区白壁3-12-13");
-			$sheet->setCellValueByColumnAndRow(20, $i, "中部産業連盟ビル新館4F");
+			$sheet->setCellValueByColumnAndRow(20, $i, "中部産業連盟ビル新館8F");
 			$sheet->setCellValueByColumnAndRow(21, $i, "(一社)日本電子機器補修協会");
-			$sheet->setCellValueByColumnAndRow(24, $i, $p_modelnum);
-			$sheet->setCellValueByColumnAndRow(25, $i, $row['option1']." ".$row['option2']." ".$row['option3']." ".$row['option4']);
-			$sheet->setCellValueByColumnAndRow(26, $i, date('Y-m-d', strtotime($row['g_buydt'])));
-			$g_locale = mb_convert_kana($row['g_locale'], "RNASK");
-			$sheet->setCellValueByColumnAndRow(27, $i, mb_substr($g_locale, 0, 16));
-			$sheet->setCellValueByColumnAndRow(66, $i ,"予約パソコン");
-			$sheet->setCellValueByColumnAndRow(67, $i, "購入方法");
-			$sheet->setCellValueByColumnAndRow(64, $i, "PREF");
-			$sheet->setCellValueByColumnAndRow(65, $i, $row['idxnum']);
+			$sheet->setCellValueByColumnAndRow(22, $i, "主食共同購入部");
+			$sheet->setCellValueByColumnAndRow(23, $i, "お米");
+			$sheet->setCellValueByColumnAndRow(24, $i, "精米倶楽部");
+			$sheet->setCellValueByColumnAndRow(25, $i, "お米の定期便");
+			$sheet->setCellValueByColumnAndRow(26, $i, $row['category']);
+			$sheet->setCellValueByColumnAndRow(27, $i, $row['weight']."kg");
+			$sheet->setCellValueByColumnAndRow(64, $i, "RC");
+			$sheet->setCellValueByColumnAndRow(65, $i ,"お米");
+			$sheet->setCellValueByColumnAndRow(66, $i, "購入方法");
+			$sheet->setCellValueByColumnAndRow(65, $i, $row['ship_idxnum']);
 			// 時間指定があれば該当する時間帯指定サービスを選択（指定なしの場合は天地無用）・項目記入
+			$sheet->setCellValueByColumnAndRow(44, $i, $row['delivery_date']);
 			$sheet->setCellValueByColumnAndRow(45, $i, $row['specified_times']);
-			$sheet->setCellValueByColumnAndRow(53, $i, $row['specified_times']);
-			if($row['p_way'] == "代引"){
-				$sheet->setCellValueByColumnAndRow(47, $i, $row['sumkin']*100);
-				//$sheet->setCellValueByColumnAndRow(49, $i, "元払代引");
-			}else{
-				//$sheet->setCellValueByColumnAndRow(49, $i, "元払い");
-			}
-			$sheet->setCellValueByColumnAndRow(49, $i, "1");
+			$sheet->setCellValueByColumnAndRow(47, $i, $row['tanka']);
+			$p_tax = $row['tanka'] * 0.08;
+			$sheet->setCellValueByColumnAndRow(48, $i, $p_tax);
 			// 指定シール設定
 			$sheet->setCellValueExplicitByColumnAndRow(51, $i, "010"); // eコレクト(全て可能)
 			$sheet->setCellValueExplicitByColumnAndRow(52, $i, "011"); // 取扱注意
+			
 			//チェックリストにデータ格納
-			$p_chksheet->setCellValueByColumnAndRow(0, $j, "□");
-			$p_chksheet->setCellValueByColumnAndRow(1, $j, $t);
-			$p_chksheet->setCellValueByColumnAndRow(2, $j, $row['name']);
-			$p_chksheet->setCellValueByColumnAndRow(3, $j, $p_modelnum);
-			$p_chksheet->setCellValueByColumnAndRow(4, $j, $row['sumkin']);
-			$p_chksheet->setCellValueByColumnAndRow(5, $j, $row['reserv']);
-			if($row['deliveryday'] <> "0000-00-00"){
-				$p_chksheet->setCellValueByColumnAndRow(6, $j, date('Y/n/j', strtotime($row['deliveryday'])));
+			$chksheet->setCellValueByColumnAndRow(0, $j, "□");
+			$chksheet->setCellValueByColumnAndRow(1, $j, $t);
+			$chksheet->setCellValueByColumnAndRow(2, $j, $row['name']);
+			$chksheet->setCellValueByColumnAndRow(3, $j, $row['category']);
+			$chksheet->setCellValueByColumnAndRow(4, $j, $row['weight']);
+			$chksheet->setCellValueByColumnAndRow(5, $j, $row['tanka']);
+			$chksheet->setCellValueByColumnAndRow(6, $j, date('Y/n/j', strtotime($row['delivery_date'])));
+			$chksheet->setCellValueByColumnAndRow(7, $j, $row['remarks']);
+			//カテゴリーが変われば改ページ挿入
+			if(($g_category <> $row['category'] || $row['weight'] <> $g_weight) && $j > 4){
+				$chksheet->setBreak('A'.$j2, PHPExcel_Worksheet::BREAK_ROW);
 			}
-			$p_chksheet->setCellValueByColumnAndRow(7, $j, $option_han[0]);
-			$p_chksheet->setCellValueByColumnAndRow(8, $j, $option_han[1]);
-			$p_chksheet->setCellValueByColumnAndRow(9, $j, $option_han[2]);
-			$p_chksheet->setCellValueByColumnAndRow(10, $j, $option_han[3]);
-			$p_chksheet->setCellValueByColumnAndRow(11, $j, date('Y/n/j', strtotime($row['g_buydt'])));
-			$p_chksheet->setCellValueByColumnAndRow(12, $j, $row['g_staff']);
-			$p_chksheet->setCellValueByColumnAndRow(13, $j, mb_convert_kana($row['g_locale'],"ksa"));
-			$p_chksheet->setCellValueByColumnAndRow(14, $j, $remarks);
+			$g_category = $row['category'];
+			$g_weight = $row['weight'];
 		}
 		//罫線をつける
-		$p_chksheet->getStyle('A5:O'.$j)->getBorders()->getAllBorders()->setBorderStyle( PHPExcel_Style_Border::BORDER_THIN );
+		$chksheet->getStyle('A5:H'.$j)->getBorders()->getAllBorders()->setBorderStyle( PHPExcel_Style_Border::BORDER_THIN );
 		//印刷範囲を指定
-		$p_chksheet->getPageSetup()->setPrintArea('A1:O'.$j);
-		$p_chksheet->setCellValueByColumnAndRow(0, 1, "譲渡会予約PC(".date('Y/n/j')." 佐川伝票発行) 発行者：".$p_staff);
-		$p_chksheet->getStyleByColumnAndRow(0, 1)->getFont()->setBold(true);
+		$chksheet->getPageSetup()->setPrintArea('A1:H'.$j);
+		$chksheet->setCellValueByColumnAndRow(0, 1, "【精米倶楽部】".date('Y/n/j')."(".$p_staff."発行)");
+		$chksheet->getStyleByColumnAndRow(0, 1)->getFont()->setBold(true);
 		//リストを作成する
 		$j=4;
-		asort($p_modelnum_list);
+		asort($category_list);
+		asort($weight_list);
 		asort($p_date_list);
-		asort($p_staff_list);
-		foreach($p_modelnum_list as $val){
-			foreach($p_date_list[$val] as $val2){
-				foreach($p_staff_list[$val][$val2] as $val3){
+		foreach($category_list as $val){
+			foreach($weight_list[$val] as $val2){
+				foreach($p_date_list[$val][$val2] as $val3){
 					$chksheet->setCellValueByColumnAndRow(15, $j, $val);
-					$chksheet->setCellValueByColumnAndRow(16, $j, date('Y/n/j',strtotime($val2)));
-					$chksheet->setCellValueByColumnAndRow(17, $j, $val3);
+					$chksheet->setCellValueByColumnAndRow(16, $j, $val3);
+					$chksheet->setCellValueByColumnAndRow(17, $j, date('Y/n/j',strtotime($val3)));
 					$chksheet->setCellValueByColumnAndRow(18, $j, $sumnum_list[$val][$val2][$val3]);
 					//セルに色をつける
 					$sheet->getStyle('P5:S'.$j)->getFill()->setFillType(PHPExcel_Style_Fill::FILL_SOLID)->getStartColor()->setARGB("ffff00");
@@ -209,10 +191,8 @@
 		//フラグを更新
 		$query = "UPDATE php_rice_shipment ";
 		$query .= " SET output_flg = 9";
-		$query .= " , response = '".$g_factory."'";
 		$query .= " , slip_staff = '".$p_staff."'";
-		$query .= " , outputdt = '".$today."'";
-		$query .= " , delivery_person = '佐川'";
+		$query .= " , outputdt = NOW()";
 		$query .= " , status = 9";
 		$query .= " WHERE output_flg = 3 ";
 		$comm->ouputlog("データ抽出 実行", $prgid, SYS_LOG_TYPE_INFO);
@@ -222,7 +202,7 @@
 		}
 		//ファイル出力
 		$book->setActiveSheetIndex(0);
-		$filename = date('ymd')."_佐川予約伝票_".$g_factory."発送.xlsx";
+		$filename = "【精米倶楽部】"date('Ymd')."_佐川伝票.xlsx";
 		header('Content-Type: application/vnd.ms-excel');
 		header('Content-Disposition: attachment;filename="' . $filename . '"');
 		header('Cache-Control: max-age=0');
@@ -404,11 +384,26 @@
 
 	/* --- データセル（td） --- */
 
-	td.tbd_td_p1 {
+	td.tbd_td_p1_l {
 	width: 200px;
-	padding: 10px 5px 7px; /* データセルのパディング（上、左右、下） */
-	border: 1px solid white;
+	padding: 1px 10px 1px; /* データセルのパディング（上、左右、下） */
+	border: none;
 	text-align: left;
+	vertical-align:middle;
+	}
+	td.tbd_td_p1_c {
+	width: 200px;
+	padding: 1px 10px 1px; /* データセルのパディング（上、左右、下） */
+	border: none;
+	text-align: center;
+	vertical-align:middle;
+	}
+	td.tbd_td_p1_r {
+	width: 200px;
+	padding: 1px 10px 1px; /* データセルのパディング（上、左右、下） */
+	border: none;
+	text-align: right;
+	vertical-align:middle;
 	}
 	td.tbd_td_p2 {
 	width: 100px;
@@ -463,7 +458,14 @@
 	<script type="text/javascript">
 		//二重登録防止後伝票出力
 		function MClickBtn(action) {
-			if(window.confirm('出荷用の伝票を出力します。')){
+			var maxrow = document.getElementById('行数').value;
+			g_num = 0;
+			for(i=1; i<=maxrow; ++i){
+				if(document.getElementById('box'+i).checked == true){
+					g_num = g_num + 1;
+				}
+			}
+			if(window.confirm('出荷用の伝票を出力します。\n'+ g_num +'件')){
 				document.forms['frm'].elements[action].click();
 				return;
 			}else{
@@ -487,62 +489,33 @@
 			}
 			});
 		});
-		//デスクトップPCチェックボックス全選択
-		$(function(){
-			var checkAll = '#checkAllDsk'; //「すべて」のチェックボックスのidを指定
-			var checkBox = 'input[name="型番デスク[]"]'; //チェックボックスのnameを指定
-			$( checkAll ).on('click', function() {
-			$( checkBox ).prop('checked', $(this).is(':checked') );
-			});
-			$( checkBox ).on( 'click', function() {
-			var boxCount = $( checkBox ).length; //全チェックボックスの数を取得
-			var checked = $( checkBox + ':checked' ).length; //チェックされているチェックボックスの数を取得
-			if( checked === boxCount ) {
-			$( checkAll ).prop( 'checked', true );
-			} else {
-			$( checkAll ).prop( 'checked', false );
-			}
-			});
-		});
-		//ノートPCチェックボックス全選択
-		$(function(){
-			var checkAll = '#checkAllNote'; //「すべて」のチェックボックスのidを指定
-			var checkBox = 'input[name="型番ノート[]"]'; //チェックボックスのnameを指定
-			$( checkAll ).on('click', function() {
-			$( checkBox ).prop('checked', $(this).is(':checked') );
-			});
-			$( checkBox ).on( 'click', function() {
-			var boxCount = $( checkBox ).length; //全チェックボックスの数を取得
-			var checked = $( checkBox + ':checked' ).length; //チェックされているチェックボックスの数を取得
-			if( checked === boxCount ) {
-			$( checkAll ).prop( 'checked', true );
-			} else {
-			$( checkAll ).prop( 'checked', false );
-			}
-			});
-		});
 		//行選択でチェック
 		function CheckRow(row) {
+			console.log("CheckRow　" + row + "行目");
 			// チェックボックスはスルー
 			if (event.target.type !== 'checkbox') {
-			document.getElementById('box'+row).click();
+				document.getElementById('box'+row).click();
 			}
 		}
-		function ChangeColor(){
-			var table = document.getElementById('TBL');
-			var maxrow = document.getElementById('行数').value
-			for(i=1; i<=maxrow; ++i){
-				if(document.getElementById('box'+i).checked == true){
-					table.rows[i].style.background = "pink";
+		function ChangeColor() {
+			console.log("ChangeColor");
+			
+			const table = document.getElementById('TBL');
+			const maxrow = parseInt(document.getElementById('行数').value, 10);
+
+			for (let i = 1; i <= maxrow; ++i) {
+				const box = document.getElementById('box' + i);
+				const falseflg = document.getElementById('falseflg' + i);
+
+				if (!table.rows[i]) continue; // 行が存在しない場合はスキップ
+
+				if (box && box.checked) {
+					table.rows[i].style.backgroundColor = "pink";
 				} else {
-					if (document.getElementById('falseflg' + i).value == 1) {
-						table.rows[i].style.background = "#ff0000";
-					} else { 
-						if (i % 2 == 0) {
-							table.rows[i].style.background = "white";
-						} else {
-							table.rows[i].style.background = "#EDEDED";
-						}
+					if (falseflg && falseflg.value == "1") {
+						table.rows[i].style.backgroundColor = "#ff0000";
+					} else {
+						table.rows[i].style.backgroundColor = (i % 2 === 0) ? "white" : "#EDEDED";
 					}
 				}
 			}
@@ -776,13 +749,13 @@
 						// ================================================
 						//----- データ抽出
 						$query = "SELECT A.ship_idxnum, A.tanka, A.category, A.weight, A.delivery_date, A.specified_times, A.output_flg";
-						$query .= " ,C.name, C.company, C.phonenum1, C.postcd1, C.postcd2, C.address1, C.address2, C.address3 ";
+						$query .= " ,C.name, C.company, C.phonenum1, C.postcd1, C.postcd2, C.address1, C.address2, C.address3, B.remarks, B.subsc_idxnum ";
 						$query .= " FROM php_rice_shipment A";
 						$query .= " LEFT OUTER JOIN php_rice_subscription B ON A.subsc_idxnum=B.subsc_idxnum ";
 						$query .= " LEFT OUTER JOIN php_rice_personal_info C ON B.personal_idxnum=C.idxnum ";
 						$query .= " WHERE A.stopflg = 0";
 						$query .= "  AND A.delivery_date BETWEEN '".$p_year.$p_month."01' AND LAST_DAY('".$p_year.$p_month."01')";
-						$query .= " ORDER BY A.output_flg, C.postcd1, C.postcd2";
+						$query .= " ORDER BY A.output_flg, A.category, A.weight, C.idxnum";
 						$comm->ouputlog("データ抽出 実行", $prgid, SYS_LOG_TYPE_INFO);
 						$comm->ouputlog($query, $prgid, SYS_LOG_TYPE_DBUG);
 						if (! $rs = $db->query($query)) {
@@ -792,17 +765,15 @@
 						while ($row = $rs->fetch_array()) {
 							++$i;
 							//明細設定
-							if($row['i_modelnum'] == ""){ ?>
-								<tr style="background-color:#ff0000;" onclick="Javascript:CheckRow(<? echo $i ?>)">
-							<? }else if (($rowcnt % 2) == 0) { ?>
+							if (($rowcnt % 2) == 0) { ?>
 								<tr onclick="Javascript:CheckRow(<? echo $i ?>)">
 							<? } else { ?>
 								<tr style="background-color:#EDEDED;" onclick="Javascript:CheckRow(<? echo $i ?>)">
 							<? }
 							$rowcnt = $rowcnt +1; ?>
-								<td style="display:none;"><?php echo $row['ship_idxnum']?></td>
+								<td style="display:none;"><?php echo $row['subsc_idxnum']?></td>
 								<!-- 印刷用チェックボックス -->
-								<td style="text-align:center; vertical-align:middle;padding:0">
+								<td class="tbd_td_p1_c" >
 									<? if($row['output_flg']>0){ ?>
 										<input id="box<? echo $i ?>" disabled="disabled" type="checkbox" value="<? echo $row['ship_idxnum'] ?>" name="outputno[]"  readonly="readonly" onchange="Javascript:ChangeColor()">
 									<? }else{ ?>
@@ -810,20 +781,20 @@
 									<? } ?>
 								</td>
 								<!-- インデックス -->
-								<td width="70" style="text-align:center; vertical-align:middle;"><?php echo str_pad($row['ship_idxnum'], 6, "0", STR_PAD_LEFT) ?></td>
+								<td width="70" class="tbd_td_p1_l"><?php echo str_pad($row['ship_idxnum'], 6, "0", STR_PAD_LEFT) ?></td>
 								</td>
 								<!-- お名前 -->
 								<td style="vertical-align:middle;">
 									<?php echo $row['name'] ?>
 								</td>
 								<!-- コース -->
-								<td style="text-align:left; vertical-align:middle;"><?php echo $row['category']; ?></td>
+								<td class="tbd_td_p1_l"><?php echo $row['category']; ?></td>
 								<!-- 量 -->
-								<td style="text-align:center; vertical-align:middle;"><?php echo $row['weight']; ?></td>
+								<td class="tbd_td_p1_c"><?php echo $row['weight']; ?></td>
 								<!-- 金額 -->
-								<td style="text-align:right; vertical-align:middle;"><?php echo number_format($row['tanka'])."円"; ?></td>
+								<td class="tbd_td_p1_r"><?php echo number_format($row['tanka'])."円"; ?></td>
 								<!-- 備考 -->
-								<td style="text-align:left; vertical-align:middle;" title="<? echo $row['remarks'] ?>"><?php echo mb_substr($row['remarks'],0,10); ?><? if(mb_strlen($row['remarks']) > 9){echo "・・・";} ?></td>
+								<td class="tbd_td_p1_l" title="<? echo $row['remarks'] ?>"><?php echo mb_substr($row['remarks'],0,10); ?><? if(mb_strlen($row['remarks']) > 9){echo "・・・";} ?></td>
 							</tr>
 						<? } ?>
 						</table>
